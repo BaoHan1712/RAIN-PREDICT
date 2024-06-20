617ca22d0f880c7de790a210ae595d80d5e1c081
BaoHan1712 <hanquocbao1712@gmail.com>
2024-06-20 11:24:42 +0700

    main

---
+++ readme.md
@@ -0,0 +1,3 @@
+<h2>THIS IS A PREDICT MODEL</h2>
+
+<div><img src="data\Predict Map.png"alt="Hello" style="width:100%;max-width:640px"></div>
---
+++ test1.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import numpy as np
+import joblib
+
+# Load model
+loaded_svr_model = joblib.load('svr_rain.pickle')
+
+# Load scaler 
+loaded_scaler = joblib.load('scaler.pickle')
+
+new_data = {
+    'precipitation': [120],
+    'water level': [0.8],
+    'Soil moisture': [60]
+}
+
+new_df = pd.DataFrame(new_data)
+
+# Chuẩn hóa data
+new_data_scaled = loaded_scaler.transform(new_df)
+
+# Predict
+new_predictions = loaded_svr_model.predict(new_data_scaled)
+binary_predictions = np.where(new_predictions >= 0.5, "Yes", "No")
+
+print("Dự đoán lũ lụt:", binary_predictions)
+print("Dự đoán % :", new_predictions)
